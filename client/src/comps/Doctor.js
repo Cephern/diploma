dfa414ca76eba52fdad75ae5c99e5784f3adc643
JagMax <gewoonmaks@gmail.com>
2021-02-15 18:08:26 +0300

    OurDoctors tab styling

--- client/src/comps/Doctor.js
+++ client/src/comps/Doctor.js
@@ -1,9 +1,16 @@
 const Doctor = ({ doctor }) => {
+  const img = require(`../images/doctors/${doctor.img}.jpg`);
+
   return (
     <div className="doctor" key={doctor.id}>
-      <p>Имя: {doctor.name}</p>
-      <p>О себе: {doctor.desc}</p>
-      <p>Лайки: {doctor.likes}</p>
+      <div className="doctor-left">
+        <img src={img.default} alt="" />
+        <p>Имя: {doctor.name}</p>
+      </div>
+      <div className="doctor-right">
+        <p>О себе: {doctor.desc}</p>
+        <p>Лайки: {doctor.likes}</p>
+      </div>
     </div>
   );
 };
